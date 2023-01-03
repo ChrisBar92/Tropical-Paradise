9ac1e2d38a6c7f9325aff09876a6b66c51a7567d
Krzysztof Bartkiewicz <krzysiekbar1@gmail.com>
2023-01-03 12:49:47 +0100

    section contact and footer finished, js added, page finished

--- js/main.js
+++ js/main.js
@@ -2,8 +2,15 @@ const navHidden = document.querySelector('.nav__hidden')
 const navBtn = document.querySelector('.burger-btn')
 const allNavItems = document.querySelectorAll('.nav__item')
 
+const navBtnBars = document.querySelector('.burger-btn__bars')
+const allSections = document.querySelectorAll('.section')
+const footerYear = document.querySelector('.footer__year')
+
+// funkcja zamykająca navbara po naciśnięciu któregoś itema
 const handleNav = () => {
 	navHidden.classList.toggle('nav__hidden--active')
+
+	navBtnBars.classList.remove('black-bars-color')
 
 	allNavItems.forEach(item => {
 		item.addEventListener('click', () => {
@@ -14,6 +21,7 @@ const handleNav = () => {
 	handleNavItemsAnimation()
 }
 
+// funkcja powowdująca opóźnienie pojawiania się navbaru po naciśnięciu hamburgera
 const handleNavItemsAnimation = () => {
 	let delayTime = 0
 	allNavItems.forEach(item => {
@@ -23,4 +31,24 @@ const handleNavItemsAnimation = () => {
 	})
 }
 
+const handleObserver = () => {
+	const currentSection = window.scrollY
+
+	allSections.forEach(section => {
+		if (section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+			navBtnBars.classList.add('black-bars-color')
+		} else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+			navBtnBars.classList.remove('black-bars-color')
+		}
+	})
+}
+
+// funkcja aktualizaująca rok w footerze
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+handleCurrentYear()
+
 navBtn.addEventListener('click', handleNav)
+window.addEventListener('scroll', handleObserver)
